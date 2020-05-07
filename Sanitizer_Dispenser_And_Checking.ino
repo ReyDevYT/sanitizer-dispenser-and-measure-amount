9c699bd1812ffc7dfe95b2ad608bfbd65df91b60
DevShreyam <ReyPi@github.com>
2020-05-07 23:10:55 +0530

    Update Sanitizer_Dispenser_And_Checking.ino

--- Sanitizer_Dispenser_And_Checking.ino
+++ Sanitizer_Dispenser_And_Checking.ino
@@ -5,105 +5,111 @@
 #define Relay 6
 #define buzzPin 7 // buzzer
 
-float time=0,distance=0;
+float time = 0, distance = 0;
 int lowLevel = 8;  // distance of lower plane from indicator sensor
 int handLevel = 15; // maximum hand limit from dispenser sensor
 bool isOK;
- 
+
 
 void setup() {
- Serial.begin(9600);
+    Serial.begin(9600);
 
- pinMode(trigger,OUTPUT);
- pinMode(echo,INPUT);
- pinMode(levelTrigger,OUTPUT);
- pinMode(levelEcho,INPUT);
- pinMode(Relay,OUTPUT);
- pinMode(buzzPin,OUTPUT);
+    pinMode(trigger, OUTPUT);
+    pinMode(echo, INPUT);
+    pinMode(levelTrigger, OUTPUT);
+    pinMode(levelEcho, INPUT);
+    pinMode(Relay, OUTPUT);
+    pinMode(buzzPin, OUTPUT);
 
- delay(2000);
- isOK = true;
- 
+    delay(2000);
+
+
 }
 void loop()
 {
-  long duration, inches, cm;
+    long duration, inches, cm;
 
-  indicate_level();
+    indicate_level();
 
- if(isOK == true){
- measure_distance();
-}
+    if (isOK == true) {
+        measure_distance();
+    }
+    else {
+       
+    }
 
 }
 
 void measure_distance()
 {
- digitalWrite(trigger,LOW);
- delayMicroseconds(2);
- digitalWrite(trigger,HIGH);
- delayMicroseconds(10);
- digitalWrite(trigger,LOW);
- delayMicroseconds(2);
- time=pulseIn(echo,HIGH);
- 
- distance=time*200/20000;
+    digitalWrite(trigger, LOW);
+    delayMicroseconds(2);
+    digitalWrite(trigger, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(trigger, LOW);
+    delayMicroseconds(2);
+    time = pulseIn(echo, HIGH);
 
- if(distance<handLevel)
- {
-   digitalWrite(Relay,HIGH);
- }
- else
- {
-   digitalWrite(Relay,LOW);
- }
+    distance = time * 200 / 20000;
 
- delay(500);
+    if (distance < handLevel)
+    {
+        digitalWrite(Relay, HIGH);
+    }
+    else
+    {
+        digitalWrite(Relay, LOW);
+    }
+
+    delay(500);
 }
 
 void indicate_level()
 {
-  long duration, inches, cm;
+    long duration, inches, cm;
 
 
 
-  pinMode(levelTrigger, OUTPUT);
-  digitalWrite(levelTrigger, LOW);
-  delayMicroseconds(2);
-  digitalWrite(levelTrigger, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(levelTrigger, LOW);
-  pinMode(levelEcho, INPUT);
-  duration = pulseIn(levelEcho, HIGH);
-  inches = microsecondsToInches(duration);
-  cm = microsecondsToCentimeters(duration);
-  Serial.print(inches);
-  Serial.print("in");
-  Serial.print(cm);
-  Serial.print("cm");
-  Serial.println();
-  delay(100);
+    pinMode(levelTrigger, OUTPUT);
+    digitalWrite(levelTrigger, LOW);
+    delayMicroseconds(2);
+    digitalWrite(levelTrigger, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(levelTrigger, LOW);
+    pinMode(levelEcho, INPUT);
+    duration = pulseIn(levelEcho, HIGH);
+    inches = microsecondsToInches(duration);
+    cm = microsecondsToCentimeters(duration);
+    Serial.print(inches);
+    Serial.print("in");
+    Serial.print(cm);
+    Serial.print("cm");
+    Serial.println();
+    delay(100);
 
- 
-  
-  if(inches<lowLevel){
-   digitalWrite(buzzPin,HIGH);
-   delay(1000);
-   digitalWrite(buzzPin,LOW);
-   delay(1000);}
-   else{
-    digitalWrite(buzzPin,LOW);
-    isOK = false;
-   }
+
+
+    if (inches > lowLevel) {
+        isOK = true;
+        digitalWrite(buzzPin, HIGH);
+        delay(1000);
+        digitalWrite(buzzPin, LOW);
+        delay(1000);
+    }
+    else {
+        isOK = false;
+        digitalWrite(buzzPin, LOW);
+        
+    }
 
 }
 
- long microsecondsToInches( long microseconds )
+long microsecondsToInches(long microseconds)
 {
- return microseconds / 74 / 2;
+    return microseconds / 74 / 2;
 }
 
-long microsecondsToCentimeters( long microseconds )
+long microsecondsToCentimeters(long microseconds)
 {
- return microseconds / 29 / 2;
+    return microseconds / 29 / 2;
 }

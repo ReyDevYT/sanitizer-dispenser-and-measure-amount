99e1c4469dd57db2add7bd1a1ce823ef973a88cc
DevShreyam <ReyPi@github.com>
2020-05-14 03:57:44 +0530

    Update Sanitizer_Dispenser_And_Checking.ino

--- Sanitizer_Dispenser_And_Checking.ino
+++ Sanitizer_Dispenser_And_Checking.ino
@@ -5,112 +5,108 @@
 #define Relay 6
 #define buzzPin 7 // buzzer
 
-float time = 0, distance = 0;
-int lowLevel = 8;  // distance of lower plane from indicator sensor
-int handLevel = 15; // maximum hand limit from dispenser sensor
-bool isOK;
-
+float time=0,distance=0;
+int lowLevel = 9;
+bool stopTime;
 
 void setup() {
-    Serial.begin(9600);
+ stopTime = false;
+ Serial.begin(9600);
 
-    pinMode(trigger, OUTPUT);
-    pinMode(echo, INPUT);
-    pinMode(levelTrigger, OUTPUT);
-    pinMode(levelEcho, INPUT);
-    pinMode(Relay, OUTPUT);
-    pinMode(buzzPin, OUTPUT);
+ pinMode(trigger,OUTPUT);
+ pinMode(echo,INPUT);
+ pinMode(Relay,OUTPUT);
+ pinMode(buzzPin,OUTPUT);
+ pinMode(levelTrigger,OUTPUT);
+ pinMode(levelEcho,INPUT);
 
-    delay(2000);
-
-
+ delay(2000);
 }
+
 void loop()
 {
-    long duration, inches, cm;
+indicate_level(); 
+measure_distance();
+ 
 
-    indicate_level();
+ 
 
-    if (isOK == true) {
-        measure_distance();
-    }
-    else {
-       
-    }
 
 }
+
 
 void measure_distance()
 {
-    digitalWrite(trigger, LOW);
-    delayMicroseconds(2);
-    digitalWrite(trigger, HIGH);
-    delayMicroseconds(10);
-    digitalWrite(trigger, LOW);
-    delayMicroseconds(2);
-    time = pulseIn(echo, HIGH);
+ digitalWrite(trigger,LOW);
+ delayMicroseconds(2);
+ digitalWrite(trigger,HIGH);
+ delayMicroseconds(10);
+ digitalWrite(trigger,LOW);
+ delayMicroseconds(2);
+ time=pulseIn(echo,HIGH);
+ 
+ distance=time*200/20000;
 
-    distance = time * 200 / 20000;
+ if(distance<15 && stopTime == true)
+ {
+   digitalWrite(Relay,HIGH);
+   delay (3000);
+   digitalWrite(Relay,LOW);
+   Serial.print("looping");
+   stopTime = false;
+  }
+ else
+ {
+   digitalWrite(Relay,LOW);
+ }
+ if(distance>15 && stopTime == false){
+  stopTime = true;
+   Serial.print("bool change");
+ }
 
-    if (distance < handLevel && isOK == true)
-    {
-        digitalWrite(Relay, HIGH);
-        delay (5000);
-    }
-    else
-    {
-        digitalWrite(Relay, LOW);
-    }
-
-    delay(500);
+ delay(500);
 }
 
 void indicate_level()
 {
-    long duration, inches, cm;
+  long duration, inches, cm;
 
 
 
-    pinMode(levelTrigger, OUTPUT);
-    digitalWrite(levelTrigger, LOW);
-    delayMicroseconds(2);
-    digitalWrite(levelTrigger, HIGH);
-    delayMicroseconds(10);
-    digitalWrite(levelTrigger, LOW);
-    pinMode(levelEcho, INPUT);
-    duration = pulseIn(levelEcho, HIGH);
-    inches = microsecondsToInches(duration);
-    cm = microsecondsToCentimeters(duration);
-    Serial.print(inches);
-    Serial.print("in");
-    Serial.print(cm);
-    Serial.print("cm");
-    Serial.println();
-    delay(100);
+  pinMode(levelTrigger, OUTPUT);
+  digitalWrite(levelTrigger, LOW);
+  delayMicroseconds(2);
+  digitalWrite(levelTrigger, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(levelTrigger, LOW);
+  pinMode(levelEcho, INPUT);
+  duration = pulseIn(levelEcho, HIGH);
+  inches = microsecondsToInches(duration);
+  cm = microsecondsToCentimeters(duration);
+  Serial.print(inches);
+  Serial.print("in");
+  Serial.print(cm);
+  Serial.print("cm");
+  Serial.println();
+  delay(100);
 
-
-
-    if (inches < lowLevel) {
-        isOK = false;
-        digitalWrite(buzzPin, HIGH);
-        delay(1000);
-        digitalWrite(buzzPin, LOW);
-        delay(1000);
-    }
-    else {
-        isOK = true;
-        digitalWrite(buzzPin, LOW);
-        
-    }
+ 
+  
+  if(inches>=lowLevel){
+   digitalWrite(buzzPin,HIGH);
+   }
+   else{
+    digitalWrite(buzzPin,LOW);
+   }
 
 }
 
-long microsecondsToInches(long microseconds)
+ long microsecondsToInches( long microseconds )
 {
-    return microseconds / 74 / 2;
+ return microseconds / 74 / 2;
 }
 
-long microsecondsToCentimeters(long microseconds)
+long microsecondsToCentimeters( long microseconds )
 {
-    return microseconds / 29 / 2;
+ return microseconds / 29 / 2;
 }
